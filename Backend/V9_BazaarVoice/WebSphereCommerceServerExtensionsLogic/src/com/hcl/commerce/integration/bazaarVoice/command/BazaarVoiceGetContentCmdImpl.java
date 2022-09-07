2780eccd16fd343e8e243307cf1b06f4fe4f909c
Sanjeev Goyal <sanjeevkumar.goyal@hcl.com>
2022-09-07 12:45:14 +0530

    HCLC-BV-9.1.10

--- Backend/V9_BazaarVoice/WebSphereCommerceServerExtensionsLogic/src/com/hcl/commerce/integration/bazaarVoice/command/BazaarVoiceGetContentCmdImpl.java
+++ Backend/V9_BazaarVoice/WebSphereCommerceServerExtensionsLogic/src/com/hcl/commerce/integration/bazaarVoice/command/BazaarVoiceGetContentCmdImpl.java
@@ -1,24 +1,5 @@
 package com.hcl.commerce.integration.bazaarVoice.command;
 
-/**
-	*==================================================
-	Copyright [2021] [HCL Technologies]
-
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-
-		http://www.apache.org/licenses/LICENSE-2.0
-
-
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
-	*==================================================
-**/
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
@@ -44,7 +25,7 @@ import com.ibm.commerce.ras.ECMessage;
 import com.ibm.commerce.ras.ECTrace;
 
 public class BazaarVoiceGetContentCmdImpl extends ControllerCommandImpl implements BazaarVoiceGetContentCmd {
-	private static final String COPYRIGHT = "Copyright [2021] [HCL Technologies]";
+	private static final String COPYRIGHT = "Copyright [2021] [HCL America, Inc.]";
 	private static final String CLASS_NAME = BazaarVoiceGetContentCmdImpl.class.getName();
 	private static final Logger LOGGER = LoggingHelper.getLogger(BazaarVoiceGetContentCmdImpl.class);
 
--- Backend/V9_BazaarVoice/WebSphereCommerceServerExtensionsLogic/src/com/hcl/commerce/integration/bazaarVoice/datawriter/BVOutputDataWriterExtend.java
+++ Backend/V9_BazaarVoice/WebSphereCommerceServerExtensionsLogic/src/com/hcl/commerce/integration/bazaarVoice/datawriter/BVOutputDataWriterExtend.java
@@ -1,23 +1,5 @@
 package com.hcl.commerce.integration.bazaarVoice.datawriter;
 
-/**
-	*==================================================
-	Copyright [2021] [HCL Technologies]
-
-	Licensed under the Apache License, Version 2.0 (the "License");
-	you may not use this file except in compliance with the License.
-	You may obtain a copy of the License at
-
-		http://www.apache.org/licenses/LICENSE-2.0
-
-
-	Unless required by applicable law or agreed to in writing, software
-	distributed under the License is distributed on an "AS IS" BASIS,
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-	See the License for the specific language governing permissions and
-	limitations under the License.
-	*==================================================
-**/
 
 import java.io.File;
 import java.io.IOException;
@@ -43,7 +25,7 @@ import org.xml.sax.SAXException;
 import com.ibm.commerce.foundation.common.util.logging.LoggingHelper;
 
 public class BVOutputDataWriterExtend {
-	private static final String COPYRIGHT = "(c) Copyright International Business Machines Corporation 1996,2008";
+	private static final String COPYRIGHT = "Copyright [2021] [HCL America, Inc.]";
 	private static final String CLASSNAME = BVOutputDataWriterExtend.class.getName();
 	private static final Logger LOGGER = LoggingHelper.getLogger(BVOutputDataWriterExtend.class);
 	

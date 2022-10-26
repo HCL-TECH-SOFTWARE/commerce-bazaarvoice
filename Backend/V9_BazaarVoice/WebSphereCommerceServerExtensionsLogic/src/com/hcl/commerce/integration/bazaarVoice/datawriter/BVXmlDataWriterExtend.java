8fad333dce03d630d2888938abd9574a146c16a5
Sanjeev Goyal <sanjeevkumar.goyal@hcl.com>
2022-10-26 16:29:52 +0530

    HCLC-BV-9.1.10
    Updated header with Apache

--- Backend/V9_BazaarVoice/WebSphereCommerceServerExtensionsLogic/src/com/hcl/commerce/integration/bazaarVoice/datawriter/BVXmlDataWriterExtend.java
+++ Backend/V9_BazaarVoice/WebSphereCommerceServerExtensionsLogic/src/com/hcl/commerce/integration/bazaarVoice/datawriter/BVXmlDataWriterExtend.java
@@ -1,5 +1,23 @@
 package com.hcl.commerce.integration.bazaarVoice.datawriter;
 
+/**
+*==================================================
+Copyright [2022] [HCL America, Inc.]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*==================================================
+**/
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
@@ -21,7 +39,7 @@ import com.ibm.commerce.foundation.dataload.object.TableDataObject;
  * @author rahulkisan.yewale
  */
 public class BVXmlDataWriterExtend extends AbstractDataWriter {
-	private static final String COPYRIGHT = "Copyright [2021] [HCL America, Inc.]";
+	private static final String COPYRIGHT = "Copyright [2022] [HCL America, Inc.]";
 	private static final String CLASSNAME = BVXmlDataWriterExtend.class.getName();
 	private static final Logger LOGGER = LoggingHelper.getLogger(BVXmlDataWriterExtend.class);
 	String fileName;
